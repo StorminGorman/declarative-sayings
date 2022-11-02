7319862035cd72edff937801de49fb849e6c56bd
Brian Gorman <brian@gorbotics.com>
2022-11-02 08:52:07 -0500

    initial commit

---
+++ bin/declarative_sayings.dart
@@ -0,0 +1,5 @@
+import 'package:declarative_sayings/declarative_sayings.dart' as declarative_sayings;
+
+void main(List<String> arguments) {
+  print('Hello world: ${declarative_sayings.calculate()}!');
+}
---
+++ test/declarative_sayings_test.dart
@@ -0,0 +1,8 @@
+import 'package:declarative_sayings/declarative_sayings.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
